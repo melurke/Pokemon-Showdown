d063157cfbf064915e4c609ab94e742a02bedc60
melurke <101022215+melurke@users.noreply.github.com>
2022-05-07 22:23:02 +0200

    Main program

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import ast
+from concurrent.futures import thread
 import functions
 
 team = []
 total_damage_dealt = [0, 0, 0, 0, 0, 0]
-physical_defenses = [0, 0, 0, 0, 0, 0]
-special_defenses = [0, 0, 0, 0, 0, 0]
-physical_attacks = [0, 0, 0, 0, 0, 0]
-special_attacks = [0, 0, 0, 0, 0, 0]
-speeds = [0, 0, 0, 0, 0, 0]
 hps = [0, 0, 0, 0, 0, 0]
+physical_attacks = [0, 0, 0, 0, 0, 0]
+physical_defenses = [0, 0, 0, 0, 0, 0]
+special_attacks = [0, 0, 0, 0, 0, 0]
+special_defenses = [0, 0, 0, 0, 0, 0]
+speeds = [0, 0, 0, 0, 0, 0]
 
 our_team = ["Ditto", "Zacian", "Zapdos", "Landorus-Therian", "Kyogre", "Grimmsnarl"]
 total_damage = [0, 0, 0, 0, 0, 0]
@@ -36,6 +37,7 @@ while True:
         pokemon[i] = ast.literal_eval(pokemon[i])
 
     name = input("Input your opposing pokemon: ")
+    enemy_hp = int(input("How much hp does your enemy have? "))/100
     
     if not name in team:
         team.append(name)
@@ -49,24 +51,27 @@ while True:
         num_of_types = int(input("How many types does the pokemon have? "))
         types = []
         stats = []
+        evs = [0, 0, 0, 0, 0, 0]
         for i in range(0, num_of_types):
             types.append(input(f"Input the {i+1}. type of the pokemon: ").upper())
         for i in range(0, 6):
             stats.append(int(input(f"Input the {i+1}. stat of the pokemon: ")))
 
-        pokemon.append([name, types, stats])
+        pokemon.append([name, types, stats, evs])
         index = -1
     else:
         for p in range(0, len(pokemon)):
             if pokemon[p][0] == name:
                 type = pokemon[p][1]
                 stats = pokemon[p][2]
+                evs = pokemon[p][3]
                 index = p
 
     print(pokemon[index])
     pokemon_list = pokemon.copy()
     types = pokemon[index][1]
     stats = pokemon[index][2]
+    evs = pokemon[index][3]
 
     for p in range(0, len(pokemon)):
         pokemon[p] = str(pokemon[p])
@@ -102,10 +107,16 @@ while True:
 
     our_pokemon = input("Which pokemon are you using? ")
 
+    if our_pokemon in ["Zapdos", "Landorus-Therian", "Grimmsnarl"]:
+        has_status_moves = True
+    else:
+        has_status_moves = False
+
     for pokemon in pokemon_list:
         if pokemon[0] == our_pokemon:
             our_types = pokemon[1]
             our_base_stats = pokemon[2]
+            our_evs = pokemon[3]
 
     our_stats = [our_hps[our_team.index(our_pokemon)], our_physical_attacks[our_team.index(our_pokemon)], our_physical_defenses[our_team.index(our_pokemon)], our_special_attacks[our_team.index(our_pokemon)], our_special_attacks[our_team.index(our_pokemon)], our_speeds[our_team.index(our_pokemon)]]
 
@@ -136,8 +147,12 @@ while True:
     stab_attacks = []
     type_attacks = []
     others_attacks = []
+    theoretical_damages = []
+    move_values = []
     others = float(input("What are the other factors (number)? "))
     weather = input("What is the weather? ").upper()
+    hp = functions.base_hp(stats[0])
+    real_hp = hp * enemy_hp
 
     for move in legal_moves:
         others = 1
@@ -174,7 +189,6 @@ while True:
         stab_attacks.append(stab)
         type_attacks.append(type)
         others_attacks.append(others)
-
         if move[4] == "SPECIAL":
             damages.append(functions.damage(our_stats[1]*our_special_attack_multiplier, stats[1]*special_defense_multiplier, stab, type, others, move[1])[0])
         elif move[4] == "PHYSICAL":
@@ -184,25 +198,109 @@ while True:
         else:
             print("Couldn't find type for the attack: " + move[0])
 
-    attack = legal_moves[damages.index(max(damages))]
+        theoretical_damages.append(damages[-1]/real_hp)
+
+    for move in legal_moves:
+        move_values.append((((damages[legal_moves.index(move)]/hp)*0.77)+1)*move[5])
+
+    attack = legal_moves[move_values.index(max(move_values))]
+    print(attack)
+
     stab = stab_attacks[damages.index(max(damages))]
     type = type_attacks[damages.index(max(damages))]
     others = others_attacks[damages.index(max(damages))]
-    print(attack)
-    print(max(damages))
+    print(attack[0] + ": " + str(int(damages[legal_moves.index(attack)])))
 
-    hp = functions.base_hp(stats[0])
     damage = float(input("How much damage did you deal? "))
-    crit = int(input("Did you do a critical hit? "))
     total_damage_dealt[team.index(name)] += damage
+    crit = int(input("Did you do a critical hit (1 or 0)? "))
+    if crit == 1:
+        real_damage = damage*hp/1.5
+    else:
+        real_damage = damage*hp
 
     if attack[4] == "PHYSICAL":
-        physical_defenses[team.index(name)] = int(functions.defense(damage*hp, our_stats[1]*our_physical_attack_multiplier, stab, type, others, attack[1]))
+        physical_defenses[team.index(name)] = int(functions.defense(real_damage, our_stats[1]*our_physical_attack_multiplier, stab, type, others, attack[1]))
         print("Enemy's physical defense: " + str(physical_defenses[team.index(name)]))
     elif attack[4] == "SPECIAL":
-        special_defenses[team.index(name)] = int(functions.defense(damage*hp, our_stats[1]*our_special_attack_multiplier, stab, type, others, attack[1]))
+        special_defenses[team.index(name)] = int(functions.defense(real_damage, our_stats[1]*our_special_attack_multiplier, stab, type, others, attack[1]))
         print("Enemy's special defense: " + str(special_defenses[team.index(name)]))
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+    if False:
+
+        
+
+        legal_moves = []
+        for move in our_moves:
+            if our_pokemon in move[-1]:
+                legal_moves.append(move)
+        damages = []
+        stab_attacks = []
+        type_attacks = []
+        others_attacks = []
+        for move in legal_moves:
+            others = 1
+
+            if weather == "RAINY":
+                if move[3] == "WATER":
+                    others *= 1.5
+                if move[3] == "FIRE":
+                    others *= 0.5
+            elif weather == "SUNNY":
+                if move[3] == "WATER":
+                    others *= 0.5
+                if move[3] == "FIRE":
+                    others *= 1.5
+
+            if move[3] in our_types:
+                stab = 1.5
+            else:
+                stab = 1
+            
+            if move[3] in effective[0]:
+                type = 4
+            elif move[3] in effective[1]:
+                type = 2
+            elif move[3] in effective[2]:
+                type = 1
+            elif move[3] in effective[3]:
+                type = 0.5
+            elif move[3] in effective[4]:
+                type = 0.25
+            else:
+                type = 0
+            
+            stab_attacks.append(stab)
+            type_attacks.append(type)
+            others_attacks.append(others)
+            if move[4] == "SPECIAL":
+                damages.append(functions.damage(our_stats[1]*our_special_attack_multiplier, special_defenses[team.index(name)]*special_defense_multiplier, stab, type, others, move[1])[0])
+            elif move[4] == "PHYSICAL":
+                damages.append(functions.damage(our_stats[1]*our_physical_attack_multiplier, physical_defenses[team.index(name)]*physical_defense_multiplier, stab, type, others, move[1])[0])
+            elif move[4] == "STATUS":
+                damages.append(0)
+            else:
+                print("Couldn't find type for the attack: " + move[0])
+
+        attack = legal_moves[damages.index(max(damages))]
+        stab = stab_attacks[damages.index(max(damages))]
+        type = type_attacks[damages.index(max(damages))]
+        others = others_attacks[damages.index(max(damages))]
+        print(attack[0] + ": " + str(int(max(damages))/hp))
+
+
 
     if functions.end_game(total_damage, total_damage_dealt):
         break
